aac03165c4f5a2fb64e3220efd2229b753d8d1ce
Addie171 <lisamgdn@gmail.com>
2023-02-27 20:21:48 +0700

    realisation of is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     #конструктор (параметры -- экземпляр драйвера и url адрес)
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # метод, который открывает нужную страницу в браузере, используя метод get()
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
