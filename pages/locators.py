b7bc7f081a39f69c5d1fd0dfab7ce58eae53779e
Addie171 <lisamgdn@gmail.com>
2023-03-06 13:45:55 +0700

    Adding page switching

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,5 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    REGISTRATION_LINK = (By.CSS_SELECTOR, "#registration_submit")
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_submit")
+    REGISTRATION_LINK = (By.NAME, "registration_submit")
+    LOGIN_LINK = (By.NAME, "login_submit")
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,26 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.locators import MainPageLocators
 
+
+def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
+    # return LoginPage(browser=self.browser, url=self.browser.current_url)
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
