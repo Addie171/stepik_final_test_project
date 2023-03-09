2c53c1de9ac14b7d0dad6450c2a3801b05218929
Addie171 <lisamgdn@gmail.com>
2023-03-09 20:20:39 +0700

    checking add to basket

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTRATION_LINK = (By.NAME, "registration_submit")
     LOGIN_LINK = (By.NAME, "login_submit")
+
+class ProductPageLocators():
+    BUY_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+    EXPECTED_BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    EXPECTED_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    ACTUAL_BOOK_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    ACTUAL_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
---
+++ pages/product_page.py
@@ -0,0 +1,38 @@
+from .locators import ProductPageLocators
+from .base_page import BasePage
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        buy_btn = self.browser.find_element(*ProductPageLocators.BUY_BTN)
+        buy_btn.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_be_right_name(self):
+        expected_name = self.browser.find_element(*ProductPageLocators.EXPECTED_BOOK_NAME).text
+        print('expected_name =', expected_name)
+        actual_name = self.browser.find_element(*ProductPageLocators.ACTUAL_BOOK_NAME).text
+        print('actual_name =', actual_name)
+        assert expected_name == actual_name, "Names are different"
+
+    def should_be_right_price(self):
+        expected_price = self.browser.find_element(*ProductPageLocators.EXPECTED_PRICE).text
+        print('expected_price =', expected_price)
+        actual_price = self.browser.find_element(*ProductPageLocators.ACTUAL_PRICE).text
+        print('actual_price =', actual_price)
+        assert expected_price == actual_price, "Prices are different"
